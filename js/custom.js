090d60ad08f15017f9cfea9a257aa28480454816
Ched28 <75325713+Ched28@users.noreply.github.com>
2022-03-05 15:44:50 +0800

    Chedrick - Update Carousel

--- js/custom.js
+++ js/custom.js
@@ -4,34 +4,38 @@ $(function(){
       
         infinite: false,
         speed: 300,
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 1,
         draggable: false,
+        dots: false,
+        ease: true,
         prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
         nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 1,
+              slidesToScroll: 1,
               infinite: true,
-              dots: true
+              dots: false
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
             }
           },
           {
             breakpoint: 480,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1
-            }
+              slidesToScroll: 1,
+              unslick: true
+            },
           }
           // You can unslick at a given breakpoint now by adding:
           // settings: "unslick"
@@ -39,4 +43,6 @@ $(function(){
         ]
       });
 
+    
+      
 });
